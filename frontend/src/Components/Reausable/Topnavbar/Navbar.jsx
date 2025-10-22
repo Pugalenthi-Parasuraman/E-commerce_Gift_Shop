1d023aa26d3ef00c728c8813fc07562b9368f22b
Pugalenthi <pugal2327@gmail.com>
2025-10-22 20:26:34 +0530

    avatar issue

--- frontend/src/Components/Reausable/Topnavbar/Navbar.jsx
+++ frontend/src/Components/Reausable/Topnavbar/Navbar.jsx
@@ -132,7 +132,7 @@ function Navbar() {
                 <figure className="w-10 h-10 rounded-full overflow-hidden border-2 border-gray-200 dark:border-gray-600">
                   <img
                     width="50"
-                    src={user.avatar ?? "./images/default_avatar.png"}
+                    src={user?.avatar ?? "./images/default_avatar.png"}
                     alt="avatar"
                     className="w-full h-full object-cover"
                   />
